cd91317aef0d4139f915a8d54129e9108aefd398
root <root@jessie.raw>
2017-09-13 17:03:51 +0000

    Isso é apenas um teste

---
+++ script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+clear
+echo
+echo 'Debian GNU/Linux 8 jessie tty1'
+echo
+echo -n 'jessie login: '
+read USER
+echo -n 'Password: '
+read -s PASS
+if [ $USER == "vagrant " ] ; then
+
+echo 'Você passou'
+
+fi
